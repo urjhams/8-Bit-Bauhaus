4a9ee8b6d1bf5328ee4043e0b171b8679fc5e837
Quan <n.dinh@tdsoftware.de>
2019-05-08 11:28:00 +0200

    scene intro

---
+++ Assets/Global Scripts/DialogManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogManager : MonoBehaviour
+{
+    public Queue<string> sentences;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
