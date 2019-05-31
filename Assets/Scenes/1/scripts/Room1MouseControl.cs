f2d59a779f1507b74e891d9877cbd60dc8449694
Quan <n.dinh@tdsoftware.de>
2019-05-31 09:33:38 +0200

    update

--- Assets/Scenes/1/scripts/Room1MouseControl.cs
+++ Assets/Scenes/1/scripts/Room1MouseControl.cs
@@ -4,35 +4,15 @@ using UnityEngine.SceneManagement;
 
 public class Room1MouseControl : GlobalMouseControl
 {
-    string[] rightArm = {"right arm 4", "right arm 1"};
-    string[] leftArm = {"left arm 4", "left arm 3", "left arm 2"};
-    string[] rightArmDetail = { "interact_right arm 4", "interact_right arm 1" };
-    string[] leftArmDetail = { "interact_left arm 4", "interact_left arm 3", "interact_left arm 2" };
-    public static Room1MouseControl instace;
-
-    void Start()
-    {
-        //this.setUpContext();
-    }
-
     private void Awake()
     {
         this.setUpContext();
-        instace = this;
-    }
-
-    private void Update()
-    {
-        updateArm();
-        checkStatues();
     }
 
     private void setUpContext()
     {
         //disable bowl without box
         Helper.getSpriteRendererOf("bowl without box").enabled = false;
-
-        updateArm();
         checkStatues();
     }
 
@@ -44,7 +24,7 @@ public class Room1MouseControl : GlobalMouseControl
                 onOffEffect("lamp_light");
                 break;
             case "statues_discover":
-                detailInteraction("InteractContainer_statues", "Schmitz", "This statues... is that the same one in the picture?");
+                this.detailInteraction("InteractContainer_statues", "Schmitz", "This statues... is that the same one in the picture?");
                 break;
             case "rooftop door_interact":
                 //TODO
@@ -53,10 +33,10 @@ public class Room1MouseControl : GlobalMouseControl
                 SceneManager.LoadScene("Room 1 basement");
                 break;
             case "painting_discover":
-                detailInteraction("InteractContainer_paint", "Schmitz", "Hmm... this picture look nice...");
+                this.detailInteraction("InteractContainer_paint", "Schmitz", "Hmm... this picture look nice...");
                 break;
             case "bowl with box_discover":
-                detailInteraction("InteractContainer_box", "Schmitz", "A box, with a puzzle...?");
+                this.detailInteraction("InteractContainer_box", "Schmitz", "A box, with a puzzle...?");
                 break;
             default:
                 break;
@@ -64,32 +44,14 @@ public class Room1MouseControl : GlobalMouseControl
 
         if (currentHover.Contains("left arm"))
         {
-            Helper.room1_LeftArm = (Helper.room1_LeftArm == leftArm.Length - 1) ? 0 : Helper.room1_LeftArm + 1;
-            print(Helper.room1_LeftArm);
+            Helper.room1_LeftArm = (Helper.room1_LeftArm == Room1Manager.leftArm.Length - 1) ? 0 : Helper.room1_LeftArm + 1;
         }
 
         if (currentHover.Contains("right arm"))
         {
-            Helper.room1_RightArm = (Helper.room1_RightArm == rightArm.Length - 1) ? 0 : Helper.room1_RightArm + 1;
-            print(Helper.room1_RightArm);
+            Helper.room1_RightArm = (Helper.room1_RightArm == Room1Manager.rightArm.Length - 1) ? 0 : Helper.room1_RightArm + 1;
         }
-        updateArm();
         checkStatues();
-    }
-
-    private void updateArm()
-    {
-        for (int i = 0; i < leftArm.Length; i++)
-        {
-            Helper.getSpriteRendererOf(leftArm[i]).enabled = (i == Helper.room1_LeftArm);
-            Helper.getSpriteRendererOf(leftArmDetail[i]).enabled = (i == Helper.room1_LeftArm);
-        }
-        for (int i = 0; i < rightArm.Length; i++)
-        {
-            Helper.getSpriteRendererOf(rightArm[i]).enabled = (i == Helper.room1_RightArm);
-            Helper.getSpriteRendererOf(rightArmDetail[i]).enabled = (i == Helper.room1_RightArm);
-        }
-
     }
 
     private void checkStatues()
@@ -104,23 +66,8 @@ public class Room1MouseControl : GlobalMouseControl
         }
     }
 
-    public static void getStatusState()
-    {
-        print("Instance");
-        instace.updateArm();
-        instace.checkStatues();
-    }
-
     private void onOffEffect(string obj)
     {
         Helper.getSpriteRendererOf(obj).enabled = !Helper.getSpriteRendererOf(obj).enabled;
-        //ladder.active = true;
-    }
-
-    public override void detailInteraction(string name, string nameText, string contentText) {
-        base.detailInteraction(name, nameText, contentText);
-        if (!Helper.inDetail) {
-            updateArm();
-        }
     }
 }
