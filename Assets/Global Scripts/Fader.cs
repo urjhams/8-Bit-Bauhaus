77d29a30164cc642de26b73b938d743c914e8f96
SA2305 <51270154+SA2305@users.noreply.github.com>
2019-06-03 09:57:41 +0200

    Create Fader.cs

---
+++ Assets/Global Scripts/Fader.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using UnityEngine;
+
+
+public class Fader : MonoBehaviour
+{
+    public CanvasGroup[] uiElement;
+
+    private void Awake()
+    {
+        Screen.SetResolution(1366, 720, false);
+    }
+
+    private void Start()
+    {
+        foreach (CanvasGroup element in uiElement)
+        {
+            element.alpha = 0;
+        }
+        this.FadeIn();
+    }
+
+    public void FadeIn()
+    {
+        foreach (CanvasGroup element in uiElement)
+            StartCoroutine(FadeCanvasGroup(element, element.alpha, 1));
+    }
+
+    public void FadeOut()
+    {
+        foreach (CanvasGroup element in uiElement)
+            StartCoroutine(FadeCanvasGroup(element, element.alpha, 0));
+    }
+
+    public IEnumerator FadeCanvasGroup(CanvasGroup cg, float start, float end, float lerpTime = 0.5f)
+    {
+        float timeStartedLerping = Time.time;
+        float timeSinceStarted = Time.time - timeStartedLerping;
+        float percentageComplete = timeSinceStarted / lerpTime;
+
+        while (true)
+        {
+            timeSinceStarted = Time.time - timeStartedLerping;
+            percentageComplete = timeSinceStarted / lerpTime;
+
+            float currentValue = Mathf.Lerp(start, end, percentageComplete);
+
+            cg.alpha = currentValue;
+
+            if (percentageComplete >= 1) break;
+
+            yield return new WaitForEndOfFrame();
+        }
+    }
+}
