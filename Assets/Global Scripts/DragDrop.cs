ef987239393b0fbe5f5d40b50396ad80ca87657c
Quan <n.dinh@tdsoftware.de>
2019-06-05 07:05:35 +0200

    puzzle constraint drag and drop

--- Assets/Global Scripts/DragDrop.cs
+++ Assets/Global Scripts/DragDrop.cs
@@ -4,16 +4,32 @@ using UnityEngine;
 
 public class DragDrop : MonoBehaviour
 {
-    public bool locked = false;
+    private bool locked = false;
     private bool selected;
+    private Vector2 initPosition;
+    private Collider2D containerCollider;
+    void Start()
+    {
+        initPosition = gameObject.transform.position;
+        containerCollider = gameObject.transform.parent.GetComponent<Collider2D>();
+    }
     void Update()
     {
-        if (selected && !locked) {
+        if (selected && !locked)
+        {
             Vector2 cursorPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             transform.position = new Vector2(cursorPosition.x, cursorPosition.y);
         }
-        if (Input.GetMouseButtonUp(0)) {
+        if (Input.GetMouseButtonUp(0))
+        {
             selected = false;
+            if (containerCollider.bounds.Contains(gameObject.GetComponent<Collider2D>().bounds.min) &&
+            containerCollider.bounds.Contains(gameObject.GetComponent<Collider2D>().bounds.max))
+            {
+                // inside container
+            } else {
+                transform.position = initPosition;
+            }
         }
     }
     void OnMouseOver()
@@ -23,14 +39,16 @@ public class DragDrop : MonoBehaviour
     }
     void OnTriggerStay2D(Collider2D other)
     {
-        if (other.gameObject.name.Equals("correct " + gameObject.name) && !selected) {
+        if (other.gameObject.name.Equals("correct " + gameObject.name) && !selected)
+        {
             transform.position = other.gameObject.transform.position;
             locked = true;
             setLayerOrder(2);
             selected = !selected;
         }
     }
-    private void setLayerOrder(int order) {
+    private void setLayerOrder(int order)
+    {
         gameObject.GetComponent<SpriteRenderer>().sortingOrder = order;
     }
 }
