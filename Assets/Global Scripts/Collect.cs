6103ef54bd6d7885871f39466d5ced34425bf50f
Quan <n.dinh@tdsoftware.de>
2019-06-12 10:34:54 +0200

    fix the inventory with mutiple items
    the items to add must be place in same coordinate

--- Assets/Global Scripts/Collect.cs
+++ Assets/Global Scripts/Collect.cs
@@ -11,19 +11,21 @@ public class Collect : MonoBehaviour
     {
         inventory = GameObject.FindGameObjectWithTag("Inventory").GetComponent<Inventory>();
     }
-void OnMouseDown()
-{
-    if (itemButton == null)
-        return;
-    for (int i = 0; i < inventory.slot.Length; i++) {
-        if (!inventory.hasItem[i]) {
-            inventory.hasItem[i] = true;
-            var color = inventory.slot[i].GetComponent<Image>().material.color;
-            color.a = 1.0f;
-            Instantiate(itemButton, inventory.slot[i].transform, false);
-            Destroy(itemButton);
-            break;
+    void OnMouseDown()
+    {
+        if (itemButton == null)
+            return;
+        for (int i = 0; i < inventory.slot.Length; i++)
+        {
+            if (!inventory.hasItem[i])
+            {
+                inventory.hasItem[i] = true;
+                var color = inventory.slot[i].GetComponent<Image>().material.color;
+                color.a = 1.0f;
+                Instantiate(itemButton, inventory.slot[i].transform, false);
+                Destroy(itemButton);
+                break;
+            }
         }
     }
-}
 }
