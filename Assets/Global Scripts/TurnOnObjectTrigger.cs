f52da3fde454e054cb0d0ce75a61d331f5c9cb01
SA2305 <51270154+SA2305@users.noreply.github.com>
2019-06-16 21:22:47 +0200

    Create TurnOnObjectTrigger.cs

---
+++ Assets/Global Scripts/TurnOnObjectTrigger.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class TurnOnObjectTrigger : MonoBehaviour
+{
+    public GameObject[] triggerObjects;
+    public virtual void OnMouseDown()
+    {
+        if (triggerObjects != null){
+            foreach (var item in triggerObjects)
+            {
+                item.SetActive(true);
+            }
+        }
+    }
+}
