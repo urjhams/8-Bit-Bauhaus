63d81f38cce048e876692484015230663914cfa2
SA2305 <51270154+SA2305@users.noreply.github.com>
2019-06-16 21:22:25 +0200

    Create selfCollect.cs

---
+++ Assets/Global Scripts/selfCollect.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class selfCollect : MonoBehaviour
+{
+    private Inventory inventory;
+    public GameObject itemButton;
+    void Start()
+    {
+        inventory = GameObject.FindGameObjectWithTag("Inventory").GetComponent<Inventory>();
+    }
+    public virtual void OnMouseDown()
+    {
+        if (itemButton == null)
+            return;
+        for (int i = 0; i < inventory.slot.Length; i++)
+        {
+            if (!inventory.hasItem[i])
+            {
+                inventory.hasItem[i] = true;
+                var color = inventory.slot[i].GetComponent<Image>().material.color;
+                color.a = 1.0f;
+                Instantiate(itemButton, inventory.slot[i].transform, false);
+                Destroy(gameObject);
+                GameManager.currentInventoryItems.Add(itemButton);
+                break;
+            }
+        }
+    }
+}
