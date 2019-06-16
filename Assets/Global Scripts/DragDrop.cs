8cc99ad2a37a14aeb00cca05b47a6bccf54c3b20
SA2305 <51270154+SA2305@users.noreply.github.com>
2019-06-16 21:21:41 +0200

    Create DragDrop.cs

---
+++ Assets/Global Scripts/DragDrop.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class DragDrop : MonoBehaviour
+{
+    [HideInInspector] public bool locked = false;
+    [HideInInspector] public bool selected;
+    protected virtual void Start()
+    {
+    }
+    protected virtual void Update()
+    {
+        if (selected && !locked)
+        {
+            Vector2 cursorPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            transform.position = new Vector2(cursorPosition.x, cursorPosition.y);
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            selected = false;
+        }
+    }
+    void OnMouseOver()
+    {
+        if (Input.GetMouseButtonDown(0))
+            selected = true;
+    }
+    protected virtual void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.gameObject.name.Equals("correct " + gameObject.name) && !selected)
+        {
+            transform.position = other.gameObject.transform.position;
+            locked = true;
+            selected = !selected;
+        }
+    }
+}
