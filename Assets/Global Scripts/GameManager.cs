d608ce06d48a0ef2070d0080c089133729221a20
SA2305 <51270154+SA2305@users.noreply.github.com>
2019-06-16 21:21:51 +0200

    Create GameManager.cs

---
+++ Assets/Global Scripts/GameManager.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager
+{
+    public static ArrayList currentInventoryItems = new ArrayList();
+    public class Room1
+    {
+        public static int currentRightArm = 0;
+        public static int currentLeftArm = 1;
+        public static string[] rightArm = {
+        "right arm 4", "right arm 2", "right arm 1" };
+        public static string[] leftArm = {
+        "left arm 4", "left arm 3", "left arm 2" };
+        public static string[] rightArmDetail = {
+        "interact_right arm 4", "interact_right arm 2", "interact_right arm 1" };
+        public static string[] leftArmDetail = {
+        "interact_left arm 4", "interact_left arm 3", "interact_left arm 2" };
+        public static bool statuesDone = false;
+        public static bool ladderDone = false;
+        public static List<string> boxCorrectPeices = new List<string>();
+        public static List<Tuple<string,Vector3>> boxPeices = new List<Tuple<string, Vector3>>();
+        public static bool birdFood = false;
+        public static bool boxOnTable = false;
+    }
+}
