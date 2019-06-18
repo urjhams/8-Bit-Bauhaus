5d6a9028d53e6365924ca8aa1ad9433bb00e7e97
Quan <n.dinh@tdsoftware.de>
2019-06-18 05:28:31 +0200

    update basement

--- Assets/Global Scripts/InventoryClick.cs
+++ Assets/Global Scripts/InventoryClick.cs
@@ -7,7 +7,6 @@ public class InventoryClick : MonoBehaviour
 {
     public GameObject inventoryFrame;
     public void onInventoryClick() {
-        print("clicked");
         inventoryFrame.SetActive(!inventoryFrame.activeSelf);
     }
 }
