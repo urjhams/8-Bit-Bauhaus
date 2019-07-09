abc9b4703dbf086f05f703d2fc5ca719c590c6ce
Quan <n.dinh@tdsoftware.de>
2019-07-09 03:10:04 +0200

    room 2 tree animations

--- Assets/Scenes/Menu/MenuFader.cs
+++ Assets/Scenes/Menu/MenuFader.cs
@@ -1,14 +1,9 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class MenuFader : GlobalEffectControl
 {
         public CanvasGroup[] uiElement;
 
-    /// <summary>
-    /// Awake is called when the script instance is being loaded.
-    /// </summary>
     void Awake()
     {
         Screen.SetResolution(1920, 1080, false);
