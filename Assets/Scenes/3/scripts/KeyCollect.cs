c33452e8dd74f7b4990eb87dc413c8c11652e91e
Quan <n.dinh@tdsoftware.de>
2019-07-10 18:25:27 +0200

    content for dialog room 3 when take the key

--- Assets/Scenes/3/scripts/KeyCollect.cs
+++ Assets/Scenes/3/scripts/KeyCollect.cs
@@ -1,8 +1,11 @@
-﻿public class KeyCollect : selfCollect
+﻿using UnityEngine.UI;
+public class KeyCollect : selfCollect
 {
+    public Text dialogContent;
     public override void OnMouseDown()
     {
         base.OnMouseDown();
+        dialogContent.text = "This key seem useful some where, probably I should back again to my appartment to take a look";
         GameManager.Room3.goal = true;
     }
 }
